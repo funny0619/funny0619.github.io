b71b4e5b41de7ce422c790b403a163be906fef3e
funny0619 <sunnyb@hku.hk>
2020-06-04 00:56:09 +0800

    Added Hand detection

--- index.js
+++ index.js
@@ -1,86 +1,84 @@
-$(document).ready(function() {
+$('document').ready(function() {
   PopulateKeys();
+})
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+let videoInterval = 100;
+let isVideo = true;
   navigator.getUserMedia = 
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia; 
-  $('#capture').on('click',TakeVideo);
-  $('#stopCapture').on('click',stopVideo);
-  $('#background').on('click',Process);
-  const video = document.querySelector('video');
-  const constraints = {
-      video: true,
-      width: 320, 
-      height: 240
-    };
-  const canvas = document.querySelector('canvas');
-  var cameraStatus = false;
-  function stopVideo() {
-    const stream = video.srcObject;
-    const tracks = stream.getTracks();
-    tracks.forEach(function(track) {
-      track.stop();
-    });
-    cameraStatus = false;
-    video.srcObject = null;
-  }
-  function TakeVideo() {
-    navigator.mediaDevices.getUserMedia(constraints).
-      then((stream) => {video.srcObject = stream});
-    cameraStatus = true;
-    video.addEventListener('playing', function() {
-      requestAnimationFrame(Process);
-    })
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia; 
+$('#capture').on('click',TakeVideo);
+$('#stopCapture').on('click',stopVideo);
+const video = document.querySelector('video');
+const constraints = {
+    video: true,
+    width: 320, 
+    height: 240
   };
-  function Process() {
-    const ctx = canvas.getContext("2d");
-    canvas.width = video.width;
-    canvas.height = video.height;
-    ctx.drawImage(video,0,0,canvas.width,canvas.height);
-    requestAnimationFrame(Process);
-  }
+
+let model;
+var cameraStatus = false;
+function stopVideo() {
+  const stream = video.srcObject;
+  const tracks = stream.getTracks();
+  tracks.forEach(function(track) {
+    track.stop();
+  });
+  handTrack.stopVideo(video)
+  isVideo = false;
+  cameraStatus = false;
+  video.srcObject = null;
+}
+const modelParams = {
+  flipHorizontal: true, // flip e.g for video  
+  maxNumBoxes: 2, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.79, // confidence threshold for predictions.
+}
+handTrack.load(modelParams).then(lmodel => {
+  model = lmodel;
 });
-/*
-function bufferPrepare(bufferval) {
-  bufferArray = [];
-  for (var i = 0; i < bufferval; i++) { //
-  bufferArray.push(new Uint8Array(width * height));
-}
-function readFrame() {
-  try {
-    context.drawImage(video, 0, 0, width, height); 
-  } catch (e) {
-  // The video may not be ready, yet.
-   return null;
-  }
-  return context.getImageData(0, 0, width, height);
- }
-function measureLightChanges(data) {
-  //Select the next frame from the buffer.
-  var buffer = bufferArray[bufferidx++ % bufferArray.length];
-  for (var i = 0, j = 0; i < buffer.length; i++, j += 4) {
-    // Determine lightness value.
-    var current = greyScale(data[j], data[j + 1], data[j + 2]); 
-    // Set color to black.
-    data[j] = data[j + 1] = data[j + 2] = 0; 
-    // Full opacity for changes.
-    data[j + 3] = 255 * lightnessHasChanged(i, current);   
-    // Store current lightness value.
-    buffer[i] = current;
-  }
-}
-function lightnessHasChanged(index, value) {
-  return bufferArray.some(function (buffer) {
-    return Math.abs(value - buffer[index]) >= thresholdsize;
+function TakeVideo() {
+  handTrack.startVideo(video).then(function (status) {
+    if (status) {
+      isVideo = true
+      runDetection()
+    }
   });
 }
-*/
+function runDetection() {
+  model.detect(video).then(predictions => {
+    model.renderPredictions(predictions, canvas, context, video);
+    if (predictions[0]) {
+      console.log(predictions);
+      osc.volume.value = -48+0.12*(210-parseFloat(predictions[0]['bbox'][1]));
+    }
+    if (isVideo) {
+      setTimeout(() => {
+        runDetection(video)
+      }, videoInterval);
+    }
+  });
+}
+
 function onOpencvReady() {
   document.getElementById('capture').innerHTML = 'Take Video';
 }
 //Sound Control
 //How to make sound more like theremin(???) email google?
+function noteDown(elem) {
+  var note = elem.dataset.note;
+  osc = new Tone.Oscillator(note, "square").toDestination();
+  osc.start();
+  osc.volume.value = -24;
+}
+function noteUp(elem) {
+  osc.stop();
+}
+
 function PopulateKeys() {
   var osc;
   var notes = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
@@ -94,17 +92,4 @@ function PopulateKeys() {
     }
   }
   document.getElementById('container').innerHTML = html;
-}
-
-function noteDown(elem) {
-  var note = elem.dataset.note;
-  osc = new Tone.Oscillator(note, "square").toDestination();
-  osc.start();
-  function onMouseOver(event) {
-    osc.volume.value = -48+0.12*(210-event.clientY);
-  }
-  document.addEventListener("mousemove", onMouseOver);
-}
-function noteUp(elem) {
-  osc.stop();
 }
